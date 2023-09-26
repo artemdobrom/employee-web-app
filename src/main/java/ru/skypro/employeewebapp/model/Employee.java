9a1fc5309c8a1e2c58efc3dd405d6c7a0170ecd5
artjom <artemdobrom@gmail.com>
2023-09-26 11:55:28 +0300

    done

--- src/main/java/ru/skypro/employeewebapp/model/Employee.java
+++ src/main/java/ru/skypro/employeewebapp/model/Employee.java
@@ -1,27 +1,22 @@
 package ru.skypro.employeewebapp.model;
 
 import java.util.Objects;
-import java.util.Random;
 
 public class Employee {
     private final String firstName;
     private final String lastName;
-    private int salary;
-    private int departmentNumber;
-    private final Random random = new Random();
+
 
     public Employee(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.salary = random.nextInt(10000) + 1000;
-        this.departmentNumber = random.nextInt(5) + 1;
+
     }
 
     public Employee(String firstName, String lastName, int salary, int departmentNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.salary = salary;
-        this.departmentNumber = departmentNumber;
+
     }
 
     public String getFirstName() {
@@ -32,21 +27,7 @@ public class Employee {
         return lastName;
     }
 
-    public int getSalary() {
-        return salary;
-    }
 
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public int getDepartmentNumber() {
-        return departmentNumber;
-    }
-
-    public void setDepartmentNumber(int departmentNumber) {
-        this.departmentNumber = departmentNumber;
-    }
 
     @Override
     public boolean equals(Object o) {
@@ -66,8 +47,6 @@ public class Employee {
         return "Employee{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", salary=" + salary +
-                ", departmentNumber=" + departmentNumber +
                 '}';
     }
 }
